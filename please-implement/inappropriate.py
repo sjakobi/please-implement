9d600d4afb7d14827d8d36bb0a6c58b6913be9d1
Simon Jakobi <simon.jakobi@gmail.com>
2014-04-02 16:27:08 +0200

    Remove handling of "inappropriate" exercises
    The app was hiding exercises that had been marked inappropriate
    for a given language on the corresponding /<repo> page.
    Reasons not to implement these exercises seem to have ceased to
    exist as per this comment:
    https://github.com/exercism/xpython/issues/3#issuecomment-39333645

--- please-implement/inappropriate.py
+++
@@ -1,4 +0,0 @@
-# Exercises that are deemed inappropriate for an exercises in a given language.
-DONT_IMPLEMENT = {'xpython': {'accumulate',
-                              'meetup',
-                              'strain'}}
